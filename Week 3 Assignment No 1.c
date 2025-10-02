d8461495560059d430c7fc551cc02144c4957c15
davy28748-innit <davy.28748@students.kyu.ac.ke>
2025-10-02 11:43:40 +0300

    Add files via upload

---
+++ Week 3 Assignment No 1.c
@@ -0,0 +1,22 @@
+/*Name:
+Registration No:
+Description:
+*/
+#include <stdio.h>
+
+int main(){
+	float attendance, avgMarks;
+	
+	printf("Enter attendance percantage:");
+	scanf("%f", &attendance);
+	
+	printf("Enter average marks:");
+	scanf("%f", &avgMarks);
+	
+	if(attendance>=75 &&avgMarks>=40){
+		printf("Eligible for exams. \n");
+	} else{
+		printf("Not eligible.\n");
+	}
+	return 0;
+}
